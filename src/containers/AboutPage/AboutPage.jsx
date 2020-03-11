7bc565ecc8d9933bdc16b9cba34e10f1c9cce68b
Charlie Richardson <Charlie.Richardson@Nology.local>
2020-03-11 08:30:52 +0000

    containers

---
+++ src/containers/AboutPage/AboutPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./AboutPage.module.scss";
+
+const AboutPage = () => {
+  return (
+    <>
+      <p>AboutPage works</p>
+    </>
+  );
+};
+
+export default AboutPage;
---
+++ src/containers/LandingPage/LandingPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./LandingPage.module.scss";
+
+const LandingPage = () => {
+  return (
+    <>
+      <p>LandingPage works</p>
+    </>
+  );
+};
+
+export default LandingPage;
